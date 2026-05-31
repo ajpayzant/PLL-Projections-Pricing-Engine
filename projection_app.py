08090a41ed7488a20e0335d3d333336398dda610
ajpayzant <ajpayzant@icloud.com>
2026-05-31 14:50:30 -0700

    Update projection_app.py

--- projection_app.py
+++ projection_app.py
@@ -29,7 +29,7 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-st.title("🥍 PLL Projection Engine")
+st.title("🥍 PLL Projections")
 st.markdown("##### Monte Carlo projection system for Premier Lacrosse League games")
 st.markdown("---")
 
